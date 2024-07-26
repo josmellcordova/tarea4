8398a3ce7c4c761f17939448e3aa42f479e62360
Luis David Chunqui Angulo <lchunquia@chinalco.com.pe>
2024-07-25 23:27:56 -0500

    Cambios

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from tratamientoDx import *
 print(sumar(1000,333))
 =======
 from ExploracionDX import *
+
+print(calcular_correlacion)
+
+grafico
+
 >>>>>>> b882d4ab9f7f1c7b5d4a56ec59aab31444e4daad
